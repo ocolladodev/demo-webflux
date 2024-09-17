85eac64fa2dcf4dd42b609bae19e6f20409290f8
Omar Augusto Collado Vargas <PEX0000433@prosegur.com>
2024-09-17 00:18:32 -0500

    first commit

---
+++ src/test/java/pe/com/omarcollado/demo_webflux/DemoWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.omarcollado.demo_webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
